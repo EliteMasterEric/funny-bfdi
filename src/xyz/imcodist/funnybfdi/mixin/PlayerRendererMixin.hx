478ea393958d7d3911683d81474465aea35ee76b
EliteMasterEric <ericmyllyoja@gmail.com>
2023-11-13 23:30:43 -0500

    New strict syntax

--- src/xyz/imcodist/funnybfdi/mixin/PlayerRendererMixin.hx
+++ src/xyz/imcodist/funnybfdi/mixin/PlayerRendererMixin.hx
@@ -4,17 +4,18 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import net.minecraft.client.renderer.entity.EntityRendererProvider.EntityRendererProvider_Context;
-import net.pickhaxe.core.Mixin;
+import net.pickhaxe.core.Mixin as PickHaxeMixin;
 import xyz.imcodist.funnybfdi.features.BFDIHeadFeature;
 
 // @:mixin({
 // 	value: net.minecraft.client.renderer.entity.player.PlayerRenderer,
 // 	priority: 1000
 // })
-@:meta(org.spongepowered.asm.mixin.Mixin(
-    value = net.minecraft.client.renderer.entity.player.PlayerRenderer,
-    priority = 1000
-))
+import org.spongepowered.asm.mixin.Mixin;
+@:strict(Mixin({
+    value: untyped net.minecraft.client.renderer.entity.player.PlayerRenderer,
+    priority: 1000
+}))
 abstract class PlayerRendererMixin // implements Mixin
 		extends net.minecraft.client.renderer.entity.LivingEntityRenderer<net.minecraft.client.player.AbstractClientPlayer,
 	net.minecraft.client.model.PlayerModel<net.minecraft.client.player.AbstractClientPlayer>>
@@ -27,6 +28,7 @@ abstract class PlayerRendererMixin // implements Mixin
     })
 	public function injectHeadFeature(ctx:EntityRendererProvider_Context, slim:Bool, ci:CallbackInfo):Void {
         FunnyBFDIMod.LOGGER.info("Head feature injected!");
+        //FunnyBFDIMod.LOGGER.info('${Type.getClassName(Mixin)}');
 		// this.addLayer(new BFDIHeadFeature<net.minecraft.client.player.AbstractClientPlayer, net.minecraft.client.model.PlayerModel<net.minecraft.client.player.AbstractClientPlayer>>(this));
 	}
 }
